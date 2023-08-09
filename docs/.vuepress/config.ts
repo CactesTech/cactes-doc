b0e97fd6161d587d8236cab7b673f11c4a913e8b
Jiapeng Li <mail@jiapeng.me>
2023-08-09 15:32:18 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import { defaultTheme  } from 'vuepress'
 
 export default defineUserConfig({
+  base: '/cactes-doc/',
   lang: 'zh-CN',
   title: 'Cactes Documentations',
   description: 'All knowledge you need to know about our products',
